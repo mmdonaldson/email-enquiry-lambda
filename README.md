b911474d31dcd5dc820bc06988ae7313b715d61f
Mitchell Donaldson <mitchell@gorepp.com>
2017-04-20 10:05:53 +1000

    Update to store in dynamo

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# email-enquiry-lambda
+# Form endpoint
 An endpoint to post email contact forms
+
+Stores the info in dynamodb
+
+Emails you the data.
+
+### To install
+* If you haven't used claudia.js before refer to [here](https://www.claudiajs.com/)
+* `npm install` to install the dependencies
+* create your environment variables in a file variables.json
+* `npm run create`
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-var ApiBuilder = require('claudia-api-builder');
-var api = new ApiBuilder();
+const ApiBuilder = require('claudia-api-builder');
+const querystring = require('querystring');
+const AWS = require('aws-sdk');
+const shortid = require('shortid');
+
+const api = new ApiBuilder();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports = api;
 
-api.post('/contact', function (request) {
+api.post('/contact', (request) => {
 	'use strict';
-	var nodemailer = require('nodemailer');
+	const nodemailer = require('nodemailer');
 	const transporter = nodemailer.createTransport({
 		service: 'Mailgun',
 		port: 465,
@@ -15,18 +20,51 @@ api.post('/contact', function (request) {
 		}
 	});
 
+	let text = "";
+	const body = querystring.parse(request.body);
+	Object.keys(body).forEach((key) => {
+	    text += key + ": " + body[key] + "\n";
+	});
+
 	const mailOptions = {
-		from: process.env.EMAIL_ADDRESS, // sender address
-		to: request.body.email,
+		from: process.env.EMAIL_USER, // sender address
+		to: process.env.EMAIL_ADDRESS,
 		subject: 'New contact form submission', // Subject line
-		text: JSON.stringify(request.body) // plaintext body
+		text // plaintext body
 	}
 
-	 return transporter.sendMail(mailOptions).then(function(error, info) {
-			if (error) {
-				return 'Hmm... Sorry there was an error. Please send as an email to team@gorepp.com instead!';
-			} else {
-				return 'Thanks we\'ll be in contact soon!';
-			}
-  });
-}, { success: 201 });
+	const time = new Date();
+	const id = shortid.generate();
+	const params = {
+		TableName: 'contact',
+		Item: {
+			id,
+			email: body.email,
+			name: body.name === '' ? null : body.name,
+			location: body.location === '' ? null : body.location,
+			phone: body.phone === '' ? null : body.phone,
+			message: body.message === '' ? null : body.message,
+			time: time.toString(),
+		}
+	};
+
+	return transporter.sendMail(mailOptions)
+		.then(function () {
+			var dynamoPromise = dynamoDb.put(params).promise()
+			return dynamoPromise.then(function (response) {
+				console.log(response);
+				return;
+			})
+			.catch(function (error) {
+				console.log(error);
+				throw error;
+			})
+		})
+		.then(function() {
+			return new api.ApiResponse('OK', {'Content-Type': 'text/plain'}, 200);
+		})
+		.catch(function (error) {
+			console.log('error: ', error);
+			return new api.ApiResponse({ error }, {'Content-Type': 'text/plain'}, 400);
+		});
+});
